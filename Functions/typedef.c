720f92cb6992ee4507eb145e2ad65aa9e7dd58e5
fikratgasimovsoftwareengineer <fikretqasimov95@gmail.com>
2022-09-28 17:00:38 +0200

    Typedef

---
+++ Functions/typedef.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct programmer{
+ 
+  int age;
+  char name[20];
+  float salary;
+
+
+} developer;
+
+developer d1;
+
+
+int main(){
+ printf("Secom %s",d1[2] );
+ printf("Age %d \n", d1.age);
+
+ scanf("%s",d1.name);  
+
+ printf("\nEnter name ");
+
+ return 0;
+
+}
