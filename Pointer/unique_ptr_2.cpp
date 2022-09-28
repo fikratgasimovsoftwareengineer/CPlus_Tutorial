827d2b38da2b9e50d6828d7eecd9bf9c88c06913
fikratgasimovsoftwareengineer <fikretqasimov95@gmail.com>
2022-09-28 15:32:08 +0200

    Unique Pointer

---
+++ Pointer/unique_ptr_2.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string>
+#include <memory>
+
+class Person{
+
+ private:
+	 int age;
+	 std::string name;
+ public:
+	 Person(int age, std::string name);
+         void* getResult();	 
+
+};
+Person::Person(int age, std::string name){
+
+  this->age = age;
+  this->name = name;
+ 
+
+}
+void* Person::getResult(){
+
+  fprintf(stdout, "Age %d", this->age);
+  fprintf(stdout, "Name %s", this->name.c_str());
+
+}
+
+int main(){
+ 
+ std::unique_ptr <Person> person(new Person(27, "Unique Pointers"));
+
+ 
+ person->getResult();
+ return 0;
+
+}
