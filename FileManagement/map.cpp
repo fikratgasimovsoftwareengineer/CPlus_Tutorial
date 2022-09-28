5e4590921f981923786f1645944b5c4e264bf384
fikratgasimovsoftwareengineer <fikretqasimov95@gmail.com>
2022-09-28 16:52:18 +0200

    Mapping

---
+++ FileManagement/map.cpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+map <int, string> sample_map;
+
+sample_map.insert(pair<int, string>(1, "one"));
+sample_map.insert(pair<int, string>(2, "two"));
+
+cout << sample_map[1] << " " << sample_map[2] << endl;
+cout << sample_map.at(1) << endl;
+
+}
+
