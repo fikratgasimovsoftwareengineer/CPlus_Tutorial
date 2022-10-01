f5722fd203d5d39f32a8e46051b7e98ca301c39f
fikratgasimovsoftwareengineer <fikretqasimov95@gmail.com>
2022-10-01 20:07:29 +0200

    Static Local and Global Variable

--- Embedded_C/myworkspace/host/static_assignment.c
+++ Embedded_C/myworkspace/host/static_assignment.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
+#include "static_additional.c"
+
+
+
 
 void display();
-void test_static();
-static int z = 3;
+//void test_static();
 
+//used in static additional file
+extern void test2_static();
+
+//void static_local();
+// this global extern
+extern int z;
+
+/*========================================*/
+/* this will not be accessible outside of this C file.*/
+//static int z = 12; 
+//==========================================
 int main(){
   
+    static int test = 5;
+
     int i;
-   
+        
     for (i=0; i < 3; i++){
           
 
   	display();
-	fprintf(stdout , "fIKRAT\n");
-        test_static();
-    }
-  
 
+	fprintf(stdout , "fIKRAT\n");
+
+	//test_static();
+
+    }
+
+    //static_local();
+    test = 123;
+    x = 12;
+
+    printf("Static Local: %d\n", test);
+
+    printf("Additional File: %d\n", x);
+
+
+    // call function
+    test2_static();
 }
+
+/*void static_local(){
+
+    test = 23;
+
+    printf("Static Local: %d\n", test); 
+
+}*/
 
 
 void display(){
@@ -27,21 +64,25 @@ void display(){
     
     x++;
     y++;
+
     // increase global static
-    z++;
-   // fprintf(stdout , "X: %d===============\n", x);
+    //z++;
+
+    // fprintf(stdout , "X: %d===============\n", x);
 
    // fprintf(stdout , "Y: %d**************\n", y);
     
-    fprintf(stdout , "Z: %d**************\n", z);
+    //fprintf(stdout , "Z: %d**************\n", z);
     
 }
 
 
-void test_static(){
+/*void test_static(){
 
     z+=5;
     fprintf(stdout , "Z_2: %d-----------------\n", z);    
 
 
-}
+}*/
+
+
